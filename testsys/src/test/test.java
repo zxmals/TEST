93c54b3092f9aa6339794a4669ea0b44a65a5d65
zxmal <Administrator@zxmal>
2016-07-15 16:13:57 +0800

    first project

---
+++ testsys/src/test/test.java
@@ -0,0 +1,17 @@
+package test;
+
+public class test{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println("1231321");
+		System.out.println(divR(100));
+	}
+
+	public static int divR(int a){
+		if(a==1)
+			return a;
+		else
+			return a+divR(a-1);
+	}
+}
