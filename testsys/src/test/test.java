6185a44869685fc0fbcabfd4479aa65daa7c2b1f
zxmal <Administrator@zxmal>
2016-07-15 16:22:26 +0800

    change test.java 2

--- testsys/src/test/test.java
+++ testsys/src/test/test.java
@@ -4,7 +4,7 @@ public class test{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1231321");//
+		System.out.println("1231321");
 		System.out.println(divR(100));
 	}
 
