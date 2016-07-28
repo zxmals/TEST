401896ef843b16d1ce03e4dbf758a2f89fa8d4f2
zxmal <Administrator@zxmal>
2016-07-28 11:35:45 +0800

    rename action

--- tcsystem/src/com/nuaa/ec/action/TeacherLoginAction.java
+++
@@ -1,9 +0,0 @@
-package com.nuaa.ec.action;
-
-public class TeacherLoginAction {
-	
-	
-	public String execute(){
-		return "success";
-	}
-}
---
+++ tcsystem/src/com/nuaa/ec/action/TeacherManageAction.java
@@ -0,0 +1,42 @@
+package com.nuaa.ec.action;
+
+import com.nuaa.ec.model.Teacher;
+
+public class TeacherManageAction {
+	private String teacherid;
+	private String password;
+	private Teacher teacher;
+
+	public String execute() {
+		return "success";
+	}
+
+	public String login() {
+		return "";
+	}
+
+	public String getTeacherid() {
+		return teacherid;
+	}
+
+	public void setTeacherid(String teacherid) {
+		this.teacherid = teacherid;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Teacher getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
+
+}
