6cb46406ee33deb3910772fd0c12aeb4c00106bb
unknown <1206788259@qq.com>
2016-07-29 21:42:12 +0800

    change code style

--- tcsystem/src/com/nuaa/ec/action/TeacherManageAction.java
+++ tcsystem/src/com/nuaa/ec/action/TeacherManageAction.java
@@ -12,7 +12,7 @@ public class TeacherManageAction implements SessionAware {
 	private String teacherid;
 	private String password;
 	private String loginstatus;
-	private Map<String, Object>session;
+	private Map<String, Object> session;
 	private Teacher teacher;
 	private TeacherLoginInfoDAO teacherlgdao = new TeacherLoginInfoDAO();
 	private TeacherDAO teacherdao = new TeacherDAO();
@@ -23,71 +23,75 @@ public class TeacherManageAction implements SessionAware {
 
 	public String login() {
 		TeacherLoginInfo teacherlg = null;
-//		Teacher teacher = null;
+		// Teacher teacher = null;
 		String loginresult = "500";
 		/*
-		 * 验证登录者是否为管理员或已经不是第一次登录
-		 * check user be or not fisrt log in
-		 * check user be or not admin 
+		 * 验证登录者是否为管理员或已经不是第一次登录 check user be or not fisrt log in check user be
+		 * or not admin
 		 */
 		teacherlg = teacherlgdao.findById(teacherid);
-		if(teacherlg!=null){
+		if (teacherlg != null) {
 			/*
 			 * 不是第一次登录。验证密码/ not fisrt login --check pwd
 			 */
-			if(password.equals(teacherlg.getPassword())){				
-				teacherlg.setLastLoginDate(new java.sql.Timestamp(new java.util.Date().getTime()));
-				teacherlg.setLoginTime(teacherlg.getLoginTime()+1);
+			if (password.equals(teacherlg.getPassword())) {
+				teacherlg.setLastLoginDate(new java.sql.Timestamp(
+						new java.util.Date().getTime()));
+				teacherlg.setLoginTime(teacherlg.getLoginTime() + 1);
 				teacherlgdao.save(teacherlg);
 				this.setTeacher(teacherdao.findById(teacherid));
 				session.put("teacher", teacher);
 				/*
 				 * 验证身份/check user level
 				 */
-				/*普通教师登录/general teacher login*/
-				if("1".equals(teacherlg.getLevel()))
+				/* 普通教师登录/general teacher login */
+				if ("1".equals(teacherlg.getLevel()))
 					loginresult = "general-t";
-				else/*管理员教师登录/admin-teacher login*/
-					if("2".equals(teacherlg.getLevel()))
-						loginresult = "admin-t";				
+				else /* 管理员教师登录/admin-teacher login */
+				if ("2".equals(teacherlg.getLevel()))
+					loginresult = "admin-t";
 				System.out.println("log in");
-			}
-			else{
-				/*登录密码错误/error pwd*/
+			} else {
+				/* 登录密码错误/error pwd */
 				this.setLoginstatus("error password");
 				loginresult = "relogin";
-				System.out.println("error password");				
+				System.out.println("error password");
 			}
-		}
-		else{
+		} else {
 			/*
 			 * 教师第一次登录/teacher first login
 			 */
 			teacher = teacherdao.findById(teacherid);
-			/*查找教师/find teacher*/
-			if(teacher!=null){
-				/*密码验证/check pwd*/
-				if(password.equals(teacher.getTeacherId())){
+			/* 查找教师/find teacher */
+			if (teacher != null) {
+				/* 密码验证/check pwd */
+				if (password.equals(teacher.getTeacherId())) {
 					System.out.println("first login,welcome!");
-					/*第一次登录 初始默认信息: level:1(普通教师) logintime: 1(登录次数)/ first login default-info:level:1(general-teacher),logintime:1*/
-					teacherlg = new TeacherLoginInfo(teacherid, teacher, password, "1", "1", 1, new java.sql.Timestamp(new java.util.Date().getTime()));
+					/*
+					 * 第一次登录 初始默认信息: level:1(普通教师) logintime: 1(登录次数)/ first
+					 * login default-info:level:1(general-teacher),logintime:1
+					 */
+					teacherlg = new TeacherLoginInfo(teacherid, teacher,
+							password, "1", "1", 1, new java.sql.Timestamp(
+									new java.util.Date().getTime()));
 					teacherlgdao.save(teacherlg);
 					session.put("teacher", teacher);
 					loginresult = "general-t";
-				}
-				else{
-					System.out.println("error password.first login password==ID");
+				} else {
+					System.out
+							.println("error password.first login password==ID");
 					this.setLoginstatus("error password.first login password==ID");
 					loginresult = "relogin";
 				}
-			}else{
+			} else {
 				System.out.println("user not found");
 				this.setLoginstatus("user not found");
 				loginresult = "relogin";
 			}
 		}
-		 //TODO NOTE: close Session / 重要: 使用完session后必须关闭Session
-		new BaseHibernateDAO().closeSession(new BaseHibernateDAO().getSession());
+		// TODO NOTE: close Session / 重要: 使用完session后必须关闭Session
+		new BaseHibernateDAO()
+				.closeSession(new BaseHibernateDAO().getSession());
 		return loginresult;
 	}
 
@@ -130,7 +134,4 @@ public class TeacherManageAction implements SessionAware {
 	public void setSession(Map<String, Object> session) {
 		this.session = session;
 	}
-	
-	
-
 }
