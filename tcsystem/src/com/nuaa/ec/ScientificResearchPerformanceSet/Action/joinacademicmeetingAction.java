047d8e0baaa3ad312d408901d16ed581feb8ccd0
unknown <1206788259@qq.com>
2016-10-11 21:43:15 +0800

    zxma ...

---
+++ tcsystem/src/com/nuaa/ec/ScientificResearchPerformanceSet/Action/joinacademicmeetingAction.java
@@ -0,0 +1,61 @@
+package com.nuaa.ec.ScientificResearchPerformanceSet.Action;
+
+import java.util.Map;
+
+import org.apache.struts2.interceptor.RequestAware;
+import org.apache.struts2.interceptor.SessionAware;
+
+public class joinacademicmeetingAction implements RequestAware, SessionAware {
+
+	private Map<String, Object> request;
+	private Map<String, Object> session;
+	private String foredate;
+	private String afterdate;
+	private Integer operstatus;
+	
+	//default method
+	public String execute(){
+		return "success";
+	}
+
+	public Map<String, Object> getRequest() {
+		return request;
+	}
+
+	public void setRequest(Map<String, Object> request) {
+		this.request = request;
+	}
+
+	public Map<String, Object> getSession() {
+		return session;
+	}
+
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+
+	public String getForedate() {
+		return foredate;
+	}
+
+	public void setForedate(String foredate) {
+		this.foredate = foredate;
+	}
+
+	public String getAfterdate() {
+		return afterdate;
+	}
+
+	public void setAfterdate(String afterdate) {
+		this.afterdate = afterdate;
+	}
+
+	public Integer getOperstatus() {
+		return operstatus;
+	}
+
+	public void setOperstatus(Integer operstatus) {
+		this.operstatus = operstatus;
+	}
+	
+}
